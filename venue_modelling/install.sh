c4b2b290b6e383c6004d5cdf619f13b31ac906d2
Nocturnalx <joedavison@btinternet.com>
2023-03-14 10:40:57 +0000

    sh script output formatting

--- venue_modelling/install.sh
+++ venue_modelling/install.sh
@@ -4,6 +4,7 @@ npm install multer
 npm install mysql
 npm install body-parser
 
+echo ""
 echo "setting up nginx"
 sudo rm /etc/nginx/sites-enabled/default
 
@@ -11,12 +12,15 @@ sudo cp conf/node.conf /etc/nginx/sites-available
 sudo ln -s /etc/nginx/sites-available/node.conf /etc/nginx/sites-enabled/
 
 sudo nginx -t
+echo ""
 echo "restarting nginx"
 sudo systemctl restart nginx
 
+echo ""
 sudo ufw allow 80
 sudo ufw allow 443
 
+echo ""
 echo "creating /etc/venue_modeling directory"
 sudo mkdir -p /etc/venue_modelling/digest/in
 sudo mkdir /etc/venue_modelling/digest/out
@@ -24,6 +28,7 @@ sudo mkdir /etc/venue_modelling/digest/temp
 
 sudo cp uninstall.sh /etc/venue_modelling
 
+echo ""
 echo "compiling"
 nvcc digest/digest.cu -o digest/vmd -I/usr/include/cppconn -L/usr/lib -lmysqlcppconn
 echo "vmd added to /bin/"
--- venue_modelling/uninstall.sh
+++ venue_modelling/uninstall.sh
@@ -1,9 +1,11 @@
 echo "deleting /etc/venue_modelling"
 sudo rm -r /etc/venue_modelling
 
+echo ""
 echo "deleting /bin/vmd"
 sudo rm /bin/vmd
 
+echo ""
 echo "reseting + restarting nginx"
 sudo rm /etc/nginx/sites-enabled/node.conf
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
@@ -11,4 +13,5 @@ sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl restart nginx
 
+echo ""
 echo "Remember to delete pm2 backgrond processes if not done already and remove firewall rules using ufw!"
